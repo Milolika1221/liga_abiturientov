a07244e4f49b3e8baadec19fc5681586f3e85b97
Milolika1221 <kanzer.kristina@gmail.com>
2026-04-14 20:15:20 +0700

    Таблица категорий - адаптивность + поле поиска

--- frontend/src/components/AdminPanel.jsx
+++ frontend/src/components/AdminPanel.jsx
@@ -2076,7 +2076,8 @@ const AdminPanel = () => {
           </div>
 
           {/* Таблица категорий достижений */}
-          <section className="admin-categories-table-section">
+          <div className="admin-stats__container">
+            <section className="admin-categories-table-section">
             <div className="admin-categories-table-header">
               <h2>Лидирующие категории</h2>
             </div>
@@ -2101,7 +2102,7 @@ const AdminPanel = () => {
                       </th>
                       <th
                           onClick={() => requestCategorySort('achievement_count')}
-                          style={{ cursor: 'pointer', userSelect: 'none', width: '200px' }}
+                          style={{ cursor: 'pointer', userSelect: 'none' }}
                           className="sortable-header"
                       >
                         Количество достижений
@@ -2127,6 +2128,7 @@ const AdminPanel = () => {
               )}
             </div>
           </section>
+          </div>
 
           {/* Категории */}
           <section className="admin-category-section">
@@ -2169,6 +2171,9 @@ const AdminPanel = () => {
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
                 />
+                <svg className="admin-search-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#7878FF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
               </div>
             </div>
             
